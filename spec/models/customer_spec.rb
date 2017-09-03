d0961f9d1352154afe53ac0da7ad39f8b96dcace
Greg Garner <gpgarner10@gmail.com>
2017-09-03 00:16:43 -0500

    change pg version to avoid errors with 0.21, add rails_help and spec_help

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "customer" do
+    it "has a name" do
+      customer = Customer.create(name: "Greg", phone_number: "1234567890")
+    end
+  end
+
 end
