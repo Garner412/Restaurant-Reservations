40acfda0e3a45227017a06194fb5587b33f1000d
Greg Garner <gpgarner10@gmail.com>
2017-09-03 00:59:03 -0500

    write test for validating creation and attributes of tables

--- spec/models/table_spec.rb
+++ spec/models/table_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Table, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "creating a table" do
+    it "does not accept table without a number" do
+      table = Table.new(num_of_seats: 6)
+      expect(table.valid?).to eq false
+    end
+
+    it "accepts table without phone number" do
+      table = Table.new(number: 1)
+      expect(table.valid?).to eq true
+    end
+
+    it "accepts table with number and number of seats" do
+      table = Table.new(number: 1, num_of_seats: 4)
+      expect(table.valid?).to eq true
+    end
+  end
+
+  describe "table" do
+    it "has a number" do
+      table = Table.create(number: 1, num_of_seats: 4)
+      expect(table.number).to eq 1
+    end
+
+    it "has a number of seats" do
+      table = Table.create(number: 1, num_of_seats: 4)
+      expect(table.num_of_seats).to eq 4
+
+    end
+  end
+
 end
