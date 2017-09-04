b0d151dd6282208b0a2720c2f48d6126911c7525
Greg Garner <gpgarner10@gmail.com>
2017-09-04 18:09:32 -0500

    update to include hours

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,7 +1,6 @@
 class Reservation < ActiveRecord::Base
-  belongs_to :customer
   belongs_to :table
 
-  validates_presence_of :name, :table, :num_of_seats_reserved, :reservation_date
+  validates_presence_of :name, :table, :num_of_seats_reserved, :reservation_date, :hour
 
 end
