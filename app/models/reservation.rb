e7a943f057d8bc8a1732d6fded11f10fa0496116
Greg Garner <gpgarner10@gmail.com>
2017-09-04 23:49:25 -0500

    add validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,8 @@ class Reservation < ActiveRecord::Base
 
   validates_presence_of :name, :table, :num_of_seats_reserved, :reservation_date, :hour
 
+  validates_inclusion_of :hour, in: 0..23
+
+  validates :num_of_seats_reserved, numericality: { greater_than_or_equal_to: 0 }
+
 end
