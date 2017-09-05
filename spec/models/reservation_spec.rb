e3ee3d9a4cddb10f68cb6935b4f0de33c210cde6
Greg Garner <gpgarner10@gmail.com>
2017-09-05 15:15:35 -0500

    write specs for model methods

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -49,8 +49,6 @@ RSpec.describe Reservation, type: :model do
       reservation = Reservation.new(name: "test", table: table, num_of_seats_reserved: 3, reservation_date: Date.new(2017,9,10), hour: 25)
       expect(reservation.valid?).to eq false
     end
-
-
   end
 
   describe "reservation" do
@@ -76,6 +74,40 @@ RSpec.describe Reservation, type: :model do
 
     it "has an hour" do
       expect(reservation.hour).to eq 1
+    end
+  end
+
+  describe "self.makes_reservation" do
+    
+    it "successfully makes reservation" do
+      reservation = {name: "Test", num_of_seats_reserved: 3, reservation_date: Date.new(2017,9,10), hour: 1}
+      table = []
+      table << Table.create!(number: 2, num_of_seats: 4)
+      expect { Reservation.make_reservation(table, reservation) }.to change {Reservation.all.count}.by(1)
+    end
+  end
+
+  describe "self.book_table" do
+
+    it "successfully books table" do
+      reservation = {name: "Test", num_of_seats_reserved: 3, reservation_date: Date.new(2017,9,10), hour: 1}
+      table = Table.new(number: 2, num_of_seats: 4)
+      expect { Reservation.book_table(reservation, table) }.to change {Reservation.all.count}.by(1)
+    end
+  end
+
+  describe "self.need_more_seats" do
+
+    it "successfully evaluates when more seats available" do
+      table = Table.new(number: 2, num_of_seats: 4)
+      seats_needed = 3
+      expect(Reservation.need_more_seats(table, seats_needed)).to eq false
+    end
+
+    it "successfully evaluates when more seats are needed" do
+      table = Table.new(number: 2, num_of_seats: 4)
+      seats_needed = 5
+      expect(Reservation.need_more_seats(table, seats_needed)).to eq true
     end
   end
 
